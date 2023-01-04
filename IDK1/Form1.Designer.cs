4d565953216309e5f4a25cd20d9ec5923473ec5a
jornexe <Jornexe@gmail.com>
2023-01-04 16:27:49 +0100

    added editor config for consistency

--- IDK1/Form1.Designer.cs
+++ IDK1/Form1.Designer.cs
@@ -32,10 +32,10 @@
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
             this.splitContainer2 = new System.Windows.Forms.SplitContainer();
             this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
+            this.b_Insert = new System.Windows.Forms.Button();
+            this.b_Update = new System.Windows.Forms.Button();
+            this.b_Delete = new System.Windows.Forms.Button();
+            this.b_Print = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
             this.tableLayoutPanel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
@@ -52,6 +52,7 @@
             this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.AutoSize = true;
             this.tableLayoutPanel1.ColumnCount = 1;
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
             this.tableLayoutPanel1.Controls.Add(this.splitContainer1, 0, 1);
@@ -59,22 +60,22 @@
             this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 12);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 2;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 90F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(954, 638);
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 36F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(610, 537);
             this.tableLayoutPanel1.TabIndex = 0;
             // 
             // splitContainer1
             // 
             this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.splitContainer1.Location = new System.Drawing.Point(3, 66);
+            this.splitContainer1.Location = new System.Drawing.Point(3, 39);
             this.splitContainer1.Name = "splitContainer1";
             // 
             // splitContainer1.Panel2
             // 
             this.splitContainer1.Panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.splitContainer1_Panel2_Paint);
-            this.splitContainer1.Size = new System.Drawing.Size(948, 569);
-            this.splitContainer1.SplitterDistance = 192;
+            this.splitContainer1.Size = new System.Drawing.Size(604, 495);
+            this.splitContainer1.SplitterDistance = 122;
             this.splitContainer1.TabIndex = 0;
             // 
             // splitContainer2
@@ -94,65 +95,65 @@
             // 
             this.splitContainer2.Panel2.Controls.Add(this.button5);
             this.splitContainer2.Panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.splitContainer2_Panel2_Paint);
-            this.splitContainer2.Size = new System.Drawing.Size(948, 57);
-            this.splitContainer2.SplitterDistance = 571;
+            this.splitContainer2.Size = new System.Drawing.Size(604, 30);
+            this.splitContainer2.SplitterDistance = 363;
             this.splitContainer2.TabIndex = 1;
             // 
             // flowLayoutPanel1
             // 
             this.flowLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.flowLayoutPanel1.Controls.Add(this.button1);
-            this.flowLayoutPanel1.Controls.Add(this.button2);
-            this.flowLayoutPanel1.Controls.Add(this.button3);
-            this.flowLayoutPanel1.Controls.Add(this.button4);
+            this.flowLayoutPanel1.Controls.Add(this.b_Insert);
+            this.flowLayoutPanel1.Controls.Add(this.b_Update);
+            this.flowLayoutPanel1.Controls.Add(this.b_Delete);
+            this.flowLayoutPanel1.Controls.Add(this.b_Print);
             this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(568, 26);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(360, 26);
             this.flowLayoutPanel1.TabIndex = 0;
             // 
-            // button1
+            // b_Insert
             // 
-            this.button1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.button1.Location = new System.Drawing.Point(3, 3);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Insert";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.b_Insert.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.b_Insert.Location = new System.Drawing.Point(3, 3);
+            this.b_Insert.Name = "b_Insert";
+            this.b_Insert.Size = new System.Drawing.Size(75, 23);
+            this.b_Insert.TabIndex = 0;
+            this.b_Insert.Text = "Insert";
+            this.b_Insert.UseVisualStyleBackColor = true;
+            this.b_Insert.Click += new System.EventHandler(this.button1_Click);
             // 
-            // button2
+            // b_Update
             // 
-            this.button2.Location = new System.Drawing.Point(84, 3);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Update";
-            this.button2.UseVisualStyleBackColor = true;
+            this.b_Update.Location = new System.Drawing.Point(84, 3);
+            this.b_Update.Name = "b_Update";
+            this.b_Update.Size = new System.Drawing.Size(75, 23);
+            this.b_Update.TabIndex = 1;
+            this.b_Update.Text = "Update";
+            this.b_Update.UseVisualStyleBackColor = true;
             // 
-            // button3
+            // b_Delete
             // 
-            this.button3.Location = new System.Drawing.Point(165, 3);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "Delete";
-            this.button3.UseVisualStyleBackColor = true;
+            this.b_Delete.Location = new System.Drawing.Point(165, 3);
+            this.b_Delete.Name = "b_Delete";
+            this.b_Delete.Size = new System.Drawing.Size(75, 23);
+            this.b_Delete.TabIndex = 2;
+            this.b_Delete.Text = "Delete";
+            this.b_Delete.UseVisualStyleBackColor = true;
             // 
-            // button4
+            // b_Print
             // 
-            this.button4.Location = new System.Drawing.Point(246, 3);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 23);
-            this.button4.TabIndex = 3;
-            this.button4.Text = "Print";
-            this.button4.UseVisualStyleBackColor = true;
+            this.b_Print.Location = new System.Drawing.Point(246, 3);
+            this.b_Print.Name = "b_Print";
+            this.b_Print.Size = new System.Drawing.Size(75, 23);
+            this.b_Print.TabIndex = 3;
+            this.b_Print.Text = "Print";
+            this.b_Print.UseVisualStyleBackColor = true;
             // 
             // button5
             // 
             this.button5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.button5.Location = new System.Drawing.Point(272, 3);
+            this.button5.Location = new System.Drawing.Point(136, 3);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(98, 23);
             this.button5.TabIndex = 0;
@@ -164,8 +165,9 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.ClientSize = new System.Drawing.Size(978, 662);
+            this.ClientSize = new System.Drawing.Size(634, 561);
             this.Controls.Add(this.tableLayoutPanel1);
+            this.MinimumSize = new System.Drawing.Size(650, 600);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -178,6 +180,7 @@
             this.splitContainer2.ResumeLayout(false);
             this.flowLayoutPanel1.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -187,10 +190,10 @@
         private SplitContainer splitContainer1;
         private SplitContainer splitContainer2;
         private FlowLayoutPanel flowLayoutPanel1;
-        private Button button1;
-        private Button button2;
-        private Button button3;
-        private Button button4;
+        private Button b_Insert;
+        private Button b_Update;
+        private Button b_Delete;
+        private Button b_Print;
         private Button button5;
     }
 }
