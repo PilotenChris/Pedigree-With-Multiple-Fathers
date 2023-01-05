fe23326cd8f920c6783bcec98cf2e2a5a00623df
PilotenChris <45621350+PilotenChris@users.noreply.github.com>
2023-01-05 21:12:48 +0100

    Created Get data for 2 tables in the db and added comments

--- IDK1/Program.cs
+++ IDK1/Program.cs
@@ -19,22 +19,27 @@ namespace IDK1
             
             if (!File.Exists(SQLMethods.dbpath)) {
                 // Creates the database if it doesn't exist
-                SQLiteConnection sqlite_conn;
-                sqlite_conn = SQLMethods.CreateConnection();
+                SQLiteConnection sqlite_conn = SQLMethods.CreateConnection();
                 CreateTable(sqlite_conn);
                 InsertData(sqlite_conn);
+                sqlite_conn.Close();
             }
+            //Debug.WriteLine(string.Join(",",(string[])SQLMethods.GetSexData().ToArray(typeof(string))));
         }
 
         
 
         static void CreateTable(SQLiteConnection conn) {
-            SQLiteCommand sqlite_cmd;
+            // Create a connection to the SQLite database
+            SQLiteCommand sqlite_cmd = conn.CreateCommand();
+
+            // Define the SQL statements to create four tables: "Color", "Sex", "Entity", and "Parent"
             string Sql = "CREATE TABLE IF NOT EXISTS Color(ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, Color VARCHAR(24) NOT NULL)";
             string Sql1 = "CREATE TABLE IF NOT EXISTS Sex(ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, Sex VARCHAR(10) NOT NULL)";
             string Sql2 = "CREATE TABLE IF NOT EXISTS Entity(ID VARCHAR(10) NOT NULL, Birth DATE NOT NULL, Sex INTEGER NOT NULL, Death DATE, Color INTEGER NOT NULL, PRIMARY KEY(ID), FOREIGN KEY (Color) REFERENCES Color(ID), FOREIGN KEY (Sex) REFERENCES Sex(ID))";
             string Sql3 = "CREATE TABLE IF NOT EXISTS Parent(ChildID VARCHAR(10) NOT NULL, ParentID VARCHAR(10), Known INTEGER NOT NULL, PRIMARY KEY(ChildID, ParentID), FOREIGN KEY (ChildID) REFERENCES Entity(ID), FOREIGN KEY (ParentID) REFERENCES Entity(ID))";
-            sqlite_cmd = conn.CreateCommand();
+            
+            // Execute each of the SQL statements
             sqlite_cmd.CommandText = Sql;
             sqlite_cmd.ExecuteNonQuery();
             sqlite_cmd.CommandText = Sql1;
@@ -46,10 +51,14 @@ namespace IDK1
         }
 
         static void InsertData(SQLiteConnection conn) {
-            SQLiteCommand sqlite_cmd;
-            sqlite_cmd = conn.CreateCommand();
+            // Create a connection to the SQLite database
+            SQLiteCommand sqlite_cmd = conn.CreateCommand();
+
+            // Insert three rows into the "Sex" table
             sqlite_cmd.CommandText = "INSERT INTO Sex (Sex) VALUES ('Unknown'),('Male'),('Female');";
             sqlite_cmd.ExecuteNonQuery();
+
+            // Insert five rows into the "Color" table
             sqlite_cmd.CommandText = "INSERT INTO Color (Color) VALUES ('Light Blue'),('Light Yellow'),('Light Green'),('Light Grey'),('Light Orange');";
             sqlite_cmd.ExecuteNonQuery();
         }
--- IDK1/SQLMethods.cs
+++ IDK1/SQLMethods.cs
@@ -1,4 +1,5 @@
-﻿using System.Data.SQLite;
+﻿using System.Collections;
+using System.Data.SQLite;
 using System.Diagnostics;
 using System.IO;
 
@@ -8,10 +9,9 @@ internal class SQLMethods
     public static readonly string dbpath = @"PedigreeDB.db";
     public static SQLiteConnection CreateConnection()
     {
-        SQLiteConnection sqlite_conn;
         // Create a new database connection:
-        sqlite_conn = new SQLiteConnection("Data Source=" + dbpath + ";foreign keys=true;Version=3;New=True;Compress=True;");
-
+        SQLiteConnection sqlite_conn = new SQLiteConnection("Data Source=" + dbpath + ";foreign keys=true;Version=3;New=True;Compress=True;");
+        
         // Open the connection:
         try
         {
@@ -23,10 +23,65 @@ internal class SQLMethods
         }
         return sqlite_conn;
     }
-    public static void GetSexData(SQLiteConnection conn) {
+    public static ArrayList GetSexData() {
+        // Create a connection to the SQLite database
+        SQLiteConnection sqlite_conn = CreateConnection();
 
+        // Declare a SQLiteDataReader object and an ArrayList
+        SQLiteDataReader sqlite_datareader;
+        ArrayList sexData = new ArrayList();
+
+        // Create a command to select all rows from the "Sex" table
+        SQLiteCommand sqlite_cmd = sqlite_conn.CreateCommand();
+        sqlite_cmd.CommandText = "SELECT * FROM Sex";
+
+        // Execute the command and store the resulting data
+        sqlite_datareader = sqlite_cmd.ExecuteReader();
+
+        // Loop through each row in the data reader
+        while (sqlite_datareader.Read()) {
+            // Get the value in the second column of the current row as a string
+            string myreader = sqlite_datareader.GetString(1);
+
+            // Add the value to the sexData ArrayList
+            sexData.Add(myreader);
+        }
+
+        // Close the database connection
+        sqlite_conn.Close();
+
+        // Return the ArrayList
+        return sexData;
     }
-    public static void GetColorData(SQLiteConnection conn) {
 
+    public static ArrayList GetColorData() {
+        // Create a connection to the SQLite database
+        SQLiteConnection sqlite_conn = CreateConnection();
+
+        // Declare a SQLiteDataReader object and an ArrayList
+        SQLiteDataReader sqlite_datareader;
+        ArrayList colorData = new ArrayList();
+
+        // Create a command to select all rows from the "Sex" table
+        SQLiteCommand sqlite_cmd = sqlite_conn.CreateCommand();
+        sqlite_cmd.CommandText = "SELECT * FROM Color";
+
+        // Execute the command and store the resulting data
+        sqlite_datareader = sqlite_cmd.ExecuteReader();
+
+        // Loop through each row in the data reader
+        while (sqlite_datareader.Read()) {
+            // Get the value in the second column of the current row as a string
+            string myreader = sqlite_datareader.GetString(1);
+
+            // Add the value to the sexData ArrayList
+            colorData.Add(myreader);
+        }
+
+        // Close the database connection
+        sqlite_conn.Close();
+
+        // Return the ArrayList
+        return colorData;
     }
 }
