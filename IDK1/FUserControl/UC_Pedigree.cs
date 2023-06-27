062e18243490b1433dbf387a9881ac6603c12c8b
jornexe <Jornexe@gmail.com>
2023-06-27 19:50:21 +0200

    worked on UC_pedigree

--- IDK1/FUserControl/UC_Pedigree.cs
+++ IDK1/FUserControl/UC_Pedigree.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
+using System.Data.Entity.Core.Objects.DataClasses;
 using System.Diagnostics;
 using System.Windows.Forms;
+using PedigreeMF.PedigreeClasses;
 
 namespace IDK1.FUserControl;
 public partial class UC_Pedigree : UserControl {
@@ -14,54 +16,75 @@ public partial class UC_Pedigree : UserControl {
     }
     private List<PedigreeFig> pedigreeTab = new List<PedigreeFig>();
 
-    private ArrayList entities = new();
+    private List<Entity> entities = new();
     private async void UpdateEntities() {
         entities.Clear();
+        //private async void UpdateEntities() {
+        //    entities.Clear();
+        //    foreach (var data in await SQLMethods.GetEntityDatabase()) {
+        //        entities.Add((data.Item1, data.Item2, SQLMethods.GetDeathFromEntity(data.Item1), data.Item3, (string)SQLMethods.GetMotherFromEntity(data.Item1),
+        //            string.Join(",", (string[])SQLMethods.GetFatherFromEntity(data.Item1).ToArray(typeof(string))), data.Item4));
+        //    }
+        //}
         foreach (var data in await SQLMethods.GetEntityDatabase()) {
-            entities.Add((data.Item1, data.Item2, SQLMethods.GetDeathFromEntity(data.Item1), data.Item3, SQLMethods.GetMotherFromEntity(data.Item1),
-                string.Join(",", (string[])SQLMethods.GetFatherFromEntity(data.Item1).ToArray(typeof(string))), data.Item4));
+            Entity entity = new Entity() {
+                Id = data.Item1,
+                BirthYear = DateTime.Parse(data.Item2).Year,
+                Death = SQLMethods.GetDeathFromEntity(data.Item1),
+                Sex = data.Item3,
+                Mother = SQLMethods.GetMotherFromEntity(data.Item1),
+                Fathers = new ArrayList(),
+                FigColor = Color.Blue
+            };
+
+            entities.Add(entity);
         }
     }
 
     private async void UpdatePedigreeFig() {
         foreach (var data in entities) {
-            if (data is Tuple<string ,string, string, string, string, string, string> tup) {
-                string id = tup.Item1;
-                int birth = DateTime.Parse(tup.Item2).Year;
-                string death = tup.Item3;
-                string sex = tup.Item4;
-                string mother = tup.Item5;
-                string father = tup.Item6; // remember to split?
-                string color = tup.Item7;
-                /*
-                if (sex == SEX1) {
-                    if (death.Length > 0) {
-                        pedigreeTab.Add(new PedigreeLivePol(1, 1, id, birth));
-                    }
-                    else {
-
-                    }
+            if (data.Sex == SEX1) {
+                if (data.Death.Length > 0) {
+                    pedigreeTab.Add(new PedigreePol(1, 1, data.Id, data.BirthYear, data.Mother, data.Fathers, true, data.FigColor));
                 }
-                else if (sex == SEX2) {
-                    if (death.Length > 0) {
-                        pedigreeTab.Add(new PedigreeLiveSqu(1, 1, id, birth));
-                    }
-                    else {
-
-                    }
+                else {
+                    pedigreeTab.Add(new PedigreePol(1, 1, data.Id, data.BirthYear, data.Mother, data.Fathers, false, data.FigColor));
                 }
-                else if (sex == SEX3) {
-                    if (death.Length > 0) {
-                        pedigreeTab.Add(new PedigreeLiveCir(1, 1, id, birth));
-                    }
-                    else {
-
-                    }
-                }
-                */
-
             }
+            else if (data.Sex == SEX2) {
+                if (data.Death.Length > 0) {
+                    pedigreeTab.Add(new PedigreeSqu(1, 1, data.Id, data.BirthYear, data.Mother, data.Fathers, true, data.FigColor));
+                }
+                else {
+                    pedigreeTab.Add(new PedigreeSqu(1, 1, data.Id, data.BirthYear, data.Mother, data.Fathers, false, data.FigColor));
+                }
+            }
+            else if (data.Sex == SEX3) {
+                if (data.Death.Length > 0) {
+                    pedigreeTab.Add(new PedigreeCir(1, 1, data.Id, data.BirthYear, data.Mother, data.Fathers, true, data.FigColor));
+                }
+                else {
+                    pedigreeTab.Add(new PedigreeCir(1, 1, data.Id, data.BirthYear, data.Mother, data.Fathers, false, data.FigColor));
+                }
+            }
+        }
+        Debug.WriteLine(pedigreeTab.Count);
+    }
 
+    private static Color ChangeColor(string color) {
+        switch (color) {
+            case "Light Blue": 
+                return Color.LightBlue;
+            case "Light Yellow": 
+                return Color.LightYellow;
+            case "Light Green":
+                return Color.LightGreen;
+            case "Light Grey":
+                return Color.LightGray;
+            case "Light Orange":
+                return Color.Orange;
+            default:
+                return Color.LightBlue;
         }
     }
 }
---
+++ IDK1/PedigreeClasses/Entity.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PedigreeMF.PedigreeClasses;
+internal class Entity {
+    public string Id {
+        get; set;
+    }
+    public int BirthYear {
+        get; set;
+    }
+    public string Death {
+        get; set;
+    }
+    public string Sex {
+        get; set;
+    }
+    public string? Mother {
+        get; set;
+    }
+    public ArrayList Fathers {
+        get; set;
+    }
+    public Color FigColor {
+        get; set;
+    }
+}
